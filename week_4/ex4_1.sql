c79d83f8fdae62f2bf486e38f2e1aff81bbf6421
kim youngchan <1999@korea.ac.kr>
2022-10-28 20:55:11 +0900

    done

--- week_4/ex4_1.sql
+++ week_4/ex4_1.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `student` (
     `name` VARCHAR(20) NOT NULL,
-    `id` CHAR(10) NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `admission_year` INT NOT NULL,
     `major` VARCHAR(20) NOT NULL,
     `phone_number` VARCHAR(11) NOT NULL,
     `address` VARCHAR(60) NOT NULL,
-    `credit_all` INT NOT NULL,
-    `credit_avg` DOUBLE NOT NULL,
-    `in_school` TINYINT(1)   NOT NULL,
-    PRIMARY KEY(`id`)
+    `credit_all` INT NOT NULL DEFAULT 0,
+    `credit_avg` DOUBLE NOT NULL DEFAULT 0.0,
+    `in_school` TINYINT(1) NOT NULL DEFAULT 1,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- week_4/messenger.sql
+++ week_4/messenger.sql
@@ -1,13 +1,54 @@
 CREATE TABLE `users` (
--- Your code here
+    `userid` CHAR(10) NOT NULL AUTO_INCREMENT,
+    `userpw` VARCHAR(20) NOT NULL,
+    `username` VARCHAR(20) NOT NULL,
+    `profile_photo` VARCHAR(100) NOT NULL,
+    `profile_bio` VARCHAR(100) NOT NULL,
+    `is_retired` TINYINT(1) NOT NULL DEFAULT 0,
+    `admission_date` DATETIME NOT NULL,
+    PRIMARY KEY(`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `channels` (
--- Your code here
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `creator_id` INT NOT NULL,
+    `link` VARCHAR(100) NOT NULL,
+    `capacity` INT NOT NULL,
+    `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
+    `creation_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`creator_id`) REFERENCES `users`(`userid`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- chats / blocks table code here
+CREATE TABLE `chats` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `content` TEXT NOT NULL,
+    `sender` INT NOT NULL,
+    `channel` INT NOT NULL,
+    `date_sent` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`sender`) REFERENCES `users`(`userid`) ON DELETE CASCADE,
+    FOREIGN KEY (`channel`) REFERENCES `channels`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `blocks` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `blocker` INT NOT NULL,
+    `blockee` INT NOT NULL,
+    `date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`blocker`) REFERENCES `users`(`userid`) ON DELETE CASCADE,
+    FOREIGN KEY (`blockee`) REFERENCES `users`(`userid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `follows` (
--- Your code here
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `follower` INT NOT NULL,
+    `followee` INT NOT NULL,
+    `date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`follower`) REFERENCES `users`(`userid`) ON DELETE CASCADE,
+    FOREIGN KEY (`followee`) REFERENCES `users`(`userid`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
